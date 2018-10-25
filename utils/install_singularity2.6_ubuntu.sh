d96999a8e3f1fd11842842e00cbe13a828c690b6
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2018-10-25 11:08:59 -0700

    Update install_singularity2.6_ubuntu.sh

--- utils/install_singularity2.6_ubuntu.sh
+++ utils/install_singularity2.6_ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y python dh-autoreconf build-essential libarchive-dev squashfs-tools net-tools git 
+sudo apt-get update
+sudo apt-get install -y python dh-autoreconf build-essential libarchive-dev squashfs-tools net-tools git 
+
 git clone -b vault/release-2.6 https://github.com/sylabs/singularity.git singularity2.6
 cd singularity2.6
 ./autogen.sh 
